cd1b4ec93adab739168b4017743b527c236a2ff6
Thomas Zipperle <thomas.zipperle@tum.de>
2019-10-24 15:30:47 +0200

    Add type hints

--- d2ix/__init__.py
+++ d2ix/__init__.py
@@ -1,11 +1,18 @@
 import os
 from pathlib import Path
+from typing import TypeVar, Dict
+
+import pandas as pd
 
 os.environ['NUMEXPR_MAX_THREADS'] = '8'
 
 p = Path(__file__)
 _LOG_CONFIG_FILE = str(p.parent.joinpath('logging.yaml'))
 _CONFIG_BASE_TECHNOLOGY = str(p.parent.joinpath('config/base_technology.yml'))
+
+D2IX_Data = TypeVar('D2IX_Data', pd.DataFrame, str, list)
+ModelPar = Dict[str, D2IX_Data]
+Data = Dict[str, D2IX_Data]
 
 from d2ix.core import Model
 from d2ix.core import PostProcess
--- d2ix/manual_parameter.py
+++ d2ix/manual_parameter.py
@@ -1,9 +1,11 @@
 import logging
+from typing import Dict
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
 
-def add_parameter_manual(parameters):
+def add_parameter_manual(parameters: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
     data = {}
     for k, v in parameters.items():
         if not v.empty:
