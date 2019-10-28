e04f2e52ce5c61c7957134a9e5ab6ab92ffbcd34
Thomas Zipperle <thomas.zipperle@tum.de>
2019-10-28 16:07:15 +0100

    Fix ModelPar type

--- d2ix/__init__.py
+++ d2ix/__init__.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import TypeVar, Dict
+from typing import Dict, Union
 
 import pandas as pd
 
@@ -10,9 +10,8 @@ p = Path(__file__)
 _LOG_CONFIG_FILE = str(p.parent.joinpath('logging.yaml'))
 _CONFIG_BASE_TECHNOLOGY = str(p.parent.joinpath('config/base_technology.yml'))
 
-D2IX_Data = TypeVar('D2IX_Data', pd.DataFrame, str, list)
-ModelPar = Dict[str, D2IX_Data]
-Data = Dict[str, D2IX_Data]
+ModelPar = Dict[str, Union[pd.DataFrame, list]]
+Data = Dict[str, Union[dict, list, str]]
 
 from d2ix.core import Model
 from d2ix.core import PostProcess
